62e6a8cee1214d42919abafbd0c39a750217e063
Filipe Molina <filipemolina@live.com>
2015-06-01 16:45:38 -0300

    Template Básico do Tema

---
+++ wp-content/themes/solucao/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="pt_BR">
+<head>
+  <meta charset="utf-8">
+
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="Solução Apoio Escolar">
+  <meta name="author" content="3A WorldWide">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
---
+++ wp-content/themes/solucao/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>Site Solução!</h1>
+
+<?php get_footer(); ?>
