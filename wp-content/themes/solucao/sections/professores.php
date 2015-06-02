eba1818eb44a87210803a83bb63ee7d6dee85e7d
Filipe Molina <filipemolina@live.com>
2015-06-02 16:27:23 -0300

    Professores

---
+++ wp-content/themes/solucao/sections/professores.php
@@ -0,0 +1,87 @@
+<section class="professores fonte-preta lato-regular">
+	
+	<div class="container">
+			
+		<!-- Título -->
+		
+		<div class="row titulo">
+	
+			<h2 class="fonte-azul lato-light">Professores</h2>
+
+			<div class="linha fundo-vermelho"></div>
+
+		</div>
+
+		<div class="row texto">
+			
+			<p class="centralizado lato-regular">Conheça um pouco mais sobre nossos professores.</p>
+
+		</div>
+
+		<!-- Fotos dos Professores -->
+
+		<div class="row fotos">
+
+			<!-- Cada Professor -->
+			
+			<div class="professor ativo col-md-3">
+				
+				<div class="foto" style="background-image: url('<?php bloginfo('template_url'); ?>/img/professores/professor_01.jpg');">
+					<div class="overlay">
+						<div class="nome">Fulano de Tal</div>
+						<div class="cargo">Professor de História</div>
+					</div>	
+				</div>
+
+			</div>
+
+			<div class="professor col-md-3">
+				
+				<div class="foto" style="background-image: url('<?php bloginfo('template_url'); ?>/img/professores/professor_02.jpg');">
+					<div class="overlay">
+						<div class="nome">Fulano de Tal</div>
+						<div class="cargo">Professor de História</div>
+					</div>	
+				</div>
+				
+			</div>
+
+			<div class="professor col-md-3">
+				
+				<div class="foto" style="background-image: url('<?php bloginfo('template_url'); ?>/img/professores/professor_03.jpg');">
+					<div class="overlay">
+						<div class="nome">Fulano de Tal</div>
+						<div class="cargo">Professor de História</div>
+					</div>
+				</div>
+					
+			</div>
+
+			<div class="professor col-md-3">
+				
+				<div class="foto" style="background-image: url('<?php bloginfo('template_url'); ?>/img/professores/professor_04.jpg');">
+					<div class="overlay">
+						<div class="nome">Fulano de Tal</div>
+						<div class="cargo">Professor de História</div>
+					</div>	
+				</div>
+				
+			</div>
+
+
+		</div>
+
+		<div class="row dados-professor">
+			
+			<h3 class="lato-bold">Carolina Brandão</h3>
+			<p class="cargo fonte-cinza">Professora de História</p>
+			<div class="formacao fonte-cinza">
+				<p>Bacharel em Direito pela Universidade Cândido Mendes (UCAM), campus Centro;</p>
+				<p>Bacharel em Licenciatura em História pela UERJ, experiência profissional na área de pesquisa e licenciatura em História.</p>
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
