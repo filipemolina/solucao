524a90ccb76b81247674647fdab2fb921f939d37
Filipe Molina <filipemolina@live.com>
2015-06-02 17:25:41 -0300

    Disciplinas

---
+++ wp-content/themes/solucao/sections/disciplinas.php
@@ -0,0 +1,40 @@
+<section class="disciplinas">
+	
+	<div class="container">
+		
+		<!-- Título -->
+		
+		<div class="row titulo">
+	
+			<h2 class="fonte-azul lato-light">Disciplinas</h2>
+
+			<div class="linha fundo-vermelho"></div>
+
+		</div>
+
+		<div class="row texto">
+			
+			<p class="centralizado lato-regular">Aulas particulares de reforço escolar para o Ensino Fundamental I e II e para o Ensino Médio.</p>
+
+			<ul class="setas col-md-8 col-md-offset-2 fonte-vermelha lato-bold">
+				
+				<li class="col-md-4"><span class="seta sp-seta"></span>Matemática</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Portguês</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Redação</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Física</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Inglês</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>História</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Ciências</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Espanhol</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Filosofia</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Geografia</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Biologia</li>
+				<li class="col-md-4"><span class="seta sp-seta"></span>Química</li>
+
+			</ul>
+
+		</div>
+
+	</div>
+
+</section>
