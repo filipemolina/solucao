12b5db7f54264a0c7c0af92cb528a01110480504
Filipe Molina <filipemolina@live.com>
2015-06-02 19:04:25 -0300

    Depoimentos

--- wp-content/themes/solucao/footer.php
+++ wp-content/themes/solucao/footer.php
@@ -1,5 +1,7 @@
 
-
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/unslider.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/scripts.js"></script>
 </body>
 </html>
---
+++ wp-content/themes/solucao/sections/depoimentos.php
@@ -0,0 +1,55 @@
+<section class="depoimentos">
+
+	<!-- Título -->
+		
+	<div class="row titulo">
+
+		<h2 class="fonte-branca lato-light">Depoimentos</h2>
+
+		<div class="linha fundo-branco"></div>
+
+	</div>
+	
+	<div class="slider">
+		
+		<ul class="container">
+			<li>
+				<div class="row">
+					<div class="col-md-1 col-md-offset-3">
+						<div class="sp-quote"></div>
+					</div>
+					<div class="col-md-4 depoimento lato-bold">Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento </div>
+					<div class="col-md-1">
+						<div class="sp-unquote"></div>
+					</div>
+				</div>
+			</li>
+
+			<li>
+				<div class="row">
+					<div class="col-md-1 col-md-offset-3">
+						<div class="sp-quote"></div>
+					</div>
+					<div class="col-md-4 depoimento lato-bold">Segundo Depoimento</div>
+					<div class="col-md-1">
+						<div class="sp-unquote"></div>
+					</div>
+				</div>
+			</li>
+
+			<li>
+				<div class="row">
+					<div class="col-md-1 col-md-offset-3">
+						<div class="sp-quote"></div>
+					</div>
+					<div class="col-md-4 depoimento lato-bold">Terceiro Depoimento</div>
+					<div class="col-md-1">
+						<div class="sp-unquote"></div>
+					</div>
+				</div>
+			</li>
+		</ul>
+
+	</div>
+
+</section>
