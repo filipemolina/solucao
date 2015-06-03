ede314e82f768bc3e8fb6b88e0ff65b854c5c663
Filipe Molina <filipemolina@live.com>
2015-06-03 12:00:29 -0300

    Finalização dos Depoimentos

--- wp-content/themes/solucao/sections/depoimentos.php
+++ wp-content/themes/solucao/sections/depoimentos.php
@@ -18,10 +18,16 @@
 					<div class="col-md-1 col-md-offset-3">
 						<div class="sp-quote"></div>
 					</div>
-					<div class="col-md-4 depoimento lato-bold">Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento </div>
+					<div class="col-md-4 depoimento lato-bold">Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento 
+						Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro Depoimento Primeiro  
+					</div>
 					<div class="col-md-1">
 						<div class="sp-unquote"></div>
 					</div>
+				</div>
+				<div class="row">
+					<div class="nome">Anna Spencer</div>
+					<div class="indentificacao">Mãe do Noah</div>
 				</div>
 			</li>
 
@@ -35,6 +41,10 @@
 						<div class="sp-unquote"></div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="nome">Anna Spencer</div>
+					<div class="indentificacao">Mãe do Noah</div>
+				</div>
 			</li>
 
 			<li>
@@ -47,9 +57,18 @@
 						<div class="sp-unquote"></div>
 					</div>
 				</div>
+				<div class="row">
+					<div class="nome">Anna Spencer</div>
+					<div class="indentificacao">Mãe do Noah</div>
+				</div>
 			</li>
 		</ul>
 
+	</div>
+
+	<div class="separador">
+		<div class="fundo-vermelho"></div>
+		<div class="fundo-roxo"></div>
 	</div>
 
 </section>
