45192fa93b9e4fb1b40a92564a9d89ab6c1a9f22
Filipe Molina <filipemolina@live.com>
2015-06-03 18:25:15 -0300

    Contato, footer e scroll do menu principal

--- wp-content/themes/solucao/js/scripts.js
+++ wp-content/themes/solucao/js/scripts.js
@@ -2,6 +2,26 @@ App = {};
 
 $(function() {
 
+	/////////////////////////////////////////////////////////////////// Scrolls do Menu Principal
+
+	$(".menu a").click(function(event) {
+
+		event.preventDefault();
+
+		var section = $(this).data('section');
+
+		console.log(section);
+
+		if(section != "")
+		{
+			$('html, body').animate({
+		        scrollTop: $("."+section).offset().top - 100
+		    }, 500);
+		}
+	});
+
+	/////////////////////////////////////////////////////////////////// Slider de Depoimentos
+
     var slider = $('.slider').unslider({
 		speed: 500,               //  The speed to animate each slide (in milliseconds)
 		delay: 3000,              //  The delay between slide animations (in milliseconds)
