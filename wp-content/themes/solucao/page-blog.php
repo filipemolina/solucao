5c1769f779d2c280db2fed1d264455935fa16ecf
Kleiton Dias <kleitondias.ti@gmail.com>
2015-06-19 11:49:09 -0300

    blog

--- wp-content/themes/solucao/page-blog.php
+++ wp-content/themes/solucao/page-blog.php
@@ -6,26 +6,39 @@
 
 			<?php if(have_posts()) : ?>
 			     <?php while(have_posts()) : the_post(); ?>
+			     	<div class="container">
 			          <div class="post"> 
 			               <div class="entry">
 			                    <?php
 			                    $myposts = get_posts(); 
 				               foreach($myposts as $post) :
 			                         //Escreve o Título do Post ?>
+									
+				                         <h2 class="lato-light title-blog fonte-azul "><?php the_title();?></h2>
+				                         <?php
+				                         echo "<br/>";
+				                         ?>
+										<div class="post-img lato-light">
+											<div class="img-do-post">
+				                         		<img src="<?php bloginfo('template_url') ?>/img/post_img.jpg" alt="">
+				                         	</div>
+										<?php
+				                         //Conteúdo do Post
+									echo '<div class="post-home">';
+				         				 the_content();
+				                         echo "<br/><br/>";
 
-			                         <h2 class="lato-light title-blog fonte-azul "><?php the_title();?></h2>
-			                         <?php
-			                         echo "<br/>";
-
-			                         //Conteúdo do Post
-			                         the_content();
-			                         echo "<br/><br/>";
-
-			                    endforeach;
+				                         echo '<a href="#" class="leia-mais lato-light">LEIA MAIS</a>';
 			                         
+			                        echo "</div>";
+			                    endforeach;
+			                     
 			                    ?>
+								
+			                  	</div>
 			              </div>
 			          </div>
+			        </div>
 			     <?php endwhile; ?>
 			<?php endif; ?>
 
